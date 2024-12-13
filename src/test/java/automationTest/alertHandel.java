6e62bf941807b0591ef7c31ca3e220e31060c52c
srinivas <srinivas@connexinfo.com>
2024-12-13 23:54:15 +0530

    first commit

--- src/test/java/automationTest/alertHandel.java
+++ src/test/java/automationTest/alertHandel.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 public class alertHandel {
-	///aegwegw
+
 	@Test
 	public void alert()
 	{
